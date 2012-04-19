5d69d50750b5142bbca51b971eb4bc8b1661c8c6
Isaac Priestley <progressions@gmail.com>
2012-04-19 17:39:31 -0500

    download link

--- app.rb
+++ app.rb
@@ -3,9 +3,18 @@ set :haml, :format => :html5
 Bundler.require
 require 'lib/context'
 
+
 get "/" do
   @context ||= Context.new
-
-  @files ||= @context.list_files(:limit => 10000)
+  @files ||= @context.list_files(:limit => 10)
   haml :index
+end
+
+get "/download" do
+  @context ||= Context.new
+  file = @context.get_file(:file_id => params["file_id"])
+  file_content = @context.get_file_content(:file_id => params["file_id"])
+
+  attachment file["file_name"]
+  file_content
 end
