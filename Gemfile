2f543c8cb0972057e210b4d44223f499d717bb9c
Isaac Priestley <progressions@gmail.com>
2012-06-20 15:01:05 -0500

    Some style

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source :rubygems
  
+gem "activesupport", :require => "active_support"
+
 gem "sinatra"
 gem "haml"
 gem "sass"
--- app.rb
+++ app.rb
@@ -3,12 +3,20 @@ set :haml, :format => :html5
 Bundler.require
 require 'lib/context'
 
-
 get "/" do
   limit = params[:limit] || 10000
   @context = Context.new
   @messages = @context.list_messages(:include_body => true, :limit => limit)
   @messages_total = @messages.length
+
+  @attachment_names = @messages.map do |message|
+    if message["files"]
+      message["files"].map do |file|
+        file["file_name"]
+      end
+    end
+  end.flatten.compact
+
   haml :index
 end
 
