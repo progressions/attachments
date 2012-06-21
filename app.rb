58046fb438edbe7dffa68895a12ee9a5822e0bee
Isaac Priestley <progressions@gmail.com>
2012-06-21 15:53:14 -0500

    view by file

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ get "/" do
   @messages = @context.list_messages(:include_body => true, :limit => limit)
   @messages_total = @messages.length
 
-  @attachment_names = @messages.map do |message|
+  @attachments = @messages.map do |message|
     if message["files"]
       message["files"].map do |file|
         file["file_name"]
@@ -18,6 +18,29 @@ get "/" do
   end.flatten.compact
 
   haml :index
+end
+
+get "/files" do
+  limit = params[:limit] || 10000
+  @context = Context.new
+  @files = @context.list_files(:include_body => true, :limit => limit)
+
+  @grouped_files = {}
+
+  @files.each do |file|
+    file_name = file["file_name"]
+
+    group = @grouped_files[file_name] || []
+
+    group << file
+    @grouped_files[file_name] = group
+  end
+
+  @filenames = @files.map do |file|
+    file["file_name"]
+  end.compact.flatten.uniq
+
+  haml :files
 end
 
 get "/download" do
